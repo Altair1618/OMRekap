1df0f45fd414030ab2d2b319b909bffae54c565c
Johanes Lee <13521148@std.stei.itb.ac.id>
2024-02-29 10:58:36 +0000

    Result page

--- app/src/main/java/com/k2_9/omrekap/MainActivity.kt
+++ app/src/main/java/com/k2_9/omrekap/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.k2_9.omrekap
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
 }
