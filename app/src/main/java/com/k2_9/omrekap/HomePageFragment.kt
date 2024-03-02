a6b8af92bdbb624a232287153db7f62de52d85e2
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2024-03-02 14:48:49 +0700

    add: ic_launcher_round

--- app/src/main/java/com/k2_9/omrekap/HomePageFragment.kt
+++ app/src/main/java/com/k2_9/omrekap/HomePageFragment.kt
@@ -1,17 +1,12 @@
 package com.k2_9.omrekap
 
-import android.graphics.LinearGradient
-import android.graphics.Shader
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
-class HomePageFragment: Fragment() {
-
+class HomePageFragment : Fragment() {
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
@@ -21,7 +16,10 @@ class HomePageFragment: Fragment() {
 		return inflater.inflate(R.layout.fragment_home_page, container, false)
 	}
 
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+	override fun onViewCreated(
+		view: View,
+		savedInstanceState: Bundle?,
+	) {
 		super.onViewCreated(view, savedInstanceState)
 	}
 }
