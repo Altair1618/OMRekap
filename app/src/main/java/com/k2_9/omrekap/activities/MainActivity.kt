16b4e398a214f558508e0402299cc7cb03a8edb3
Johanes Lee <13521148@std.stei.itb.ac.id>
2024-03-04 10:49:36 +0000

    Fix shadow for api level 28

--- app/src/main/java/com/k2_9/omrekap/activities/MainActivity.kt
+++ app/src/main/java/com/k2_9/omrekap/activities/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.k2_9.omrekap.activities
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.k2_9.omrekap.R
 import com.k2_9.omrekap.fragments.HomePageFragment
 import com.k2_9.omrekap.fragments.ResultPageFragment
@@ -17,5 +19,13 @@ class MainActivity : AppCompatActivity(), ResultPageFragment.OnButtonClickListen
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		val galleryCardView: CardView = findViewById(R.id.gallery_card_view)
+		val cameraCardView: CardView = findViewById(R.id.camera_card_view)
+
+		if (Build.VERSION.SDK_INT == Build.VERSION_CODES.P) {
+			galleryCardView.elevation = 0f
+			cameraCardView.elevation = 0f
+		}
 	}
 }
