47615c20f1390cb0f18fefc6d6d1f53d5857e266
Michael Utama <michaelu670@gmail.com>
2024-04-17 10:02:36 +0700

    refactor: preprocess

--- app/src/main/java/com/k2_9/omrekap/utils/PreprocessHelper.kt
+++ app/src/main/java/com/k2_9/omrekap/utils/PreprocessHelper.kt
@@ -1,32 +1,32 @@
 package com.k2_9.omrekap.utils
 
 import android.graphics.Bitmap
+import com.k2_9.omrekap.data.models.ImageSaveData
 import org.opencv.android.Utils
 import org.opencv.core.Core
 import org.opencv.core.Mat
 import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
 
-class PreprocessHelper {
-	companion object {
-		const val FINAL_WIDTH = 540
-		const val FINAL_HEIGHT = 960
-	}
-	fun preprocess(img: Bitmap): Bitmap {
+object PreprocessHelper {
+	const val FINAL_WIDTH = 540
+	const val FINAL_HEIGHT = 960
+
+	fun preprocessImage(img: Bitmap): Bitmap {
 		val mat = Mat()
 		Utils.bitmapToMat(img, mat)
-		val resultMat = preprocess(mat)
+		val resultMat = preprocessImage(mat)
 		val resultBitmap = Bitmap.createBitmap(resultMat.width(), resultMat.height(), Bitmap.Config.ARGB_8888)
 		Utils.matToBitmap(resultMat, resultBitmap)
 		return resultBitmap
 	}
-	fun preprocess(img: Mat): Mat {
+	fun preprocessImage(img: Mat): Mat {
 		return img.apply {
-			resize(this, FINAL_WIDTH.toDouble(), FINAL_HEIGHT.toDouble())
+			resizeMat(this, FINAL_WIDTH.toDouble(), FINAL_HEIGHT.toDouble())
 			normalize(this)
 		}
 	}
-	fun resize(img: Mat, width: Double, height: Double): Mat {
+	fun resizeMat(img: Mat, width: Double, height: Double): Mat {
 		val resizedImg = Mat()
 		Imgproc.resize(img, resizedImg, Size(width, height))
 		return resizedImg
