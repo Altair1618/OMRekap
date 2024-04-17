ce9a88b4415aa212d7969c52a5de981ec6c5178a
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2024-04-17 22:33:38 +0700

    add: annotate img annotation for corner

--- app/src/main/java/com/k2_9/omrekap/utils/ImageAnnotationHelper.kt
+++ app/src/main/java/com/k2_9/omrekap/utils/ImageAnnotationHelper.kt
@@ -1,6 +1,7 @@
 package com.k2_9.omrekap.utils
 
 import android.graphics.Bitmap
+import com.k2_9.omrekap.data.models.CornerPoints
 import org.opencv.android.Utils
 import org.opencv.core.Mat
 import org.opencv.core.MatOfPoint
@@ -9,13 +10,13 @@ import org.opencv.core.Scalar
 import org.opencv.imgproc.Imgproc
 
 object ImageAnnotationHelper {
-	fun annotateCorner(img: Bitmap, cornerPoints: List<MatOfPoint>): Bitmap {
+	fun annotateCorner(img: Bitmap, cornerPoints: CornerPoints): Bitmap {
 		val imgMat = Mat()
 		Utils.bitmapToMat(img, imgMat)
-		cornerPoints.forEach{
-			Imgproc.polylines(imgMat, it, true, Scalar(255.0, 0.0, 0.0), 5)
-
-		}
+		Imgproc.circle(imgMat,cornerPoints.topLeft, 10, Scalar(0.0, 255.0, 0.0), 5)
+		Imgproc.circle(imgMat,cornerPoints.topRight, 10, Scalar(0.0, 255.0, 0.0), 5)
+		Imgproc.circle(imgMat,cornerPoints.bottomLeft, 10, Scalar(0.0, 255.0, 0.0), 5)
+		Imgproc.circle(imgMat,cornerPoints.bottomRight, 10, Scalar(0.0, 255.0, 0.0), 5)
 		val annotatedImg = Bitmap.createBitmap(imgMat.width(), imgMat.height(), Bitmap.Config.ARGB_8888)
 		Utils.matToBitmap(imgMat, annotatedImg)
 		return annotatedImg
