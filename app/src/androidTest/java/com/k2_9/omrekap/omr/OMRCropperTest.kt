2da3d3e6a3921ff949c6fb67969aca6a7248660d
Altair1618 <farhannabilsuryono3@gmail.com>
2024-04-28 09:59:03 +0700

    refactor: omr testing

--- app/src/androidTest/java/com/k2_9/omrekap/omr/OMRCropperTest.kt
+++ app/src/androidTest/java/com/k2_9/omrekap/omr/OMRCropperTest.kt
@@ -5,14 +5,18 @@ import android.graphics.Bitmap
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.k2_9.omrekap.R
+import com.k2_9.omrekap.data.configs.omr.CircleTemplateLoader
 import com.k2_9.omrekap.data.configs.omr.OMRCropper
-import com.k2_9.omrekap.data.configs.omr.OMRCropperConfig
 import com.k2_9.omrekap.data.configs.omr.OMRSection
 import com.k2_9.omrekap.utils.SaveHelper
+import com.k2_9.omrekap.utils.omr.OMRConfigDetector
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
+import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
 
 @RunWith(AndroidJUnit4::class)
 class OMRCropperTest {
@@ -25,33 +29,54 @@ class OMRCropperTest {
 		appContext = InstrumentationRegistry.getInstrumentation().targetContext
 
 		// Load the image resource as a Bitmap
-		val image = Utils.loadResource(appContext, R.raw.example)
+		val imageMat = Utils.loadResource(appContext, R.raw.example)
+		val templateLoader = CircleTemplateLoader(appContext, R.raw.circle_template)
 
-		val sectionPositions =
-			mapOf(
-				OMRSection.FIRST to Pair(780, 375),
-				OMRSection.SECOND to Pair(0, 0),
-				OMRSection.THIRD to Pair(0, 0),
-			)
+		// Convert if image is not grayscale
+		val grayscaleImageMat = if (imageMat.channels() == 3) {
+			val grayImageMat = Mat()
+			Imgproc.cvtColor(imageMat, grayImageMat, Imgproc.COLOR_BGR2GRAY)
+			grayImageMat
+		} else {
+			imageMat
+		}
 
-		val config =
-			OMRCropperConfig(
-				image,
-				Pair(140, 225),
-				sectionPositions,
-			)
+		runBlocking {
+			// Get OMR Config by AprilTag
+			OMRConfigDetector.loadConfiguration(appContext)
+			val configResult = OMRConfigDetector.detectConfiguration(grayscaleImageMat)
+			assert(configResult != null)
 
-		cropper = OMRCropper(config)
+			val config = configResult!!.first
+
+			config.contourOMRHelperConfig.omrCropper.config.setImage(grayscaleImageMat)
+			config.templateMatchingOMRHelperConfig.omrCropper.config.setImage(grayscaleImageMat)
+			config.templateMatchingOMRHelperConfig.setTemplate(templateLoader)
+
+			cropper = OMRCropper(config.contourOMRHelperConfig.omrCropper.config)
+		}
 	}
 
 	@Test
 	fun test_crop() {
-		val result = cropper.crop(OMRSection.FIRST)
+		val resultFirst = cropper.crop(OMRSection.FIRST)
+		val resultSecond = cropper.crop(OMRSection.SECOND)
+		val resultThird = cropper.crop(OMRSection.THIRD)
 
-		val bitmap = Bitmap.createBitmap(result.cols(), result.rows(), Bitmap.Config.ARGB_8888)
-		Utils.matToBitmap(result, bitmap)
+		val bitmapFirst = Bitmap.createBitmap(resultFirst.cols(), resultFirst.rows(), Bitmap.Config.ARGB_8888)
+		val bitmapSecond = Bitmap.createBitmap(resultSecond.cols(), resultSecond.rows(), Bitmap.Config.ARGB_8888)
+		val bitmapThird = Bitmap.createBitmap(resultThird.cols(), resultThird.rows(), Bitmap.Config.ARGB_8888)
 
-		SaveHelper.saveImage(appContext, bitmap, "test", "test_crop.png")
-		assert(result.width() == 140 && result.height() == 225)
+		Utils.matToBitmap(resultFirst, bitmapFirst)
+		Utils.matToBitmap(resultSecond, bitmapSecond)
+		Utils.matToBitmap(resultThird, bitmapThird)
+
+		SaveHelper.saveImage(appContext, bitmapFirst, "test", "test_crop_first.png")
+		SaveHelper.saveImage(appContext, bitmapSecond, "test", "test_crop_second.png")
+		SaveHelper.saveImage(appContext, bitmapThird, "test", "test_crop_third.png")
+
+		assert(resultFirst.width() == 80 && resultFirst.height() == 124)
+		assert(resultSecond.width() == 80 && resultSecond.height() == 124)
+		assert(resultThird.width() == 80 && resultThird.height() == 124)
 	}
 }
